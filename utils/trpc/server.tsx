0c0cfa5cb129dcce05906d84fd18258f406e1dfe
Harry Yep <git@hode.co.uk>
2025-01-09 17:44:22 +0800

    feat: trpc (#5)

---
+++ utils/trpc/server.tsx
@@ -0,0 +1,23 @@
+import { auth } from '@/auth';
+import { createCaller, createTRPCContext } from '@/trpc';
+import { headers } from 'next/headers';
+import { cache } from 'react';
+
+import 'server-only';
+
+/**
+ * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
+ * handling a tRPC call from a React Server Component.
+ */
+const createContext = cache(async () => {
+  const heads = new Headers(await headers());
+
+  heads.set('x-trpc-source', 'rsc');
+
+  return createTRPCContext({
+    session: await auth(),
+    headers: heads,
+  });
+});
+
+export const api = createCaller(createContext);
