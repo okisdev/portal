fafeec767f6839cd9e3f61168b17240f53321458
Harry Yep <git@hode.co.uk>
2025-02-26 12:34:02 +0800

    fix: flow

--- lib/payment.ts
+++ lib/payment.ts
@@ -2,5 +2,5 @@ import Stripe from 'stripe';
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 export const stripe = new Stripe(process.env.STRIPE_KEY!, {
-  apiVersion: '2025-01-27.acacia',
+  apiVersion: '2025-02-24.acacia',
 });
