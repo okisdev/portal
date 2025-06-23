0fad5e2af31e6312972158cf8b0bdfee44b10941
Harry Yep <git@hode.co.uk>
2025-06-23 19:39:49 +0800

    refactor: data transfer

--- drizzle/0001_stale_mantis.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "authenticator" RENAME COLUMN "credentialID" TO "credentialId";--> statement-breakpoint
-ALTER TABLE "authenticator" DROP CONSTRAINT "authenticator_credentialID_unique";--> statement-breakpoint
-ALTER TABLE "user" ADD COLUMN "password" text;--> statement-breakpoint
-ALTER TABLE "authenticator" ADD CONSTRAINT "authenticator_credentialID_unique" UNIQUE("credentialId");
